69a0f4b32f6b437497dd6c46056a75c67b03917f
fedhere <fedhere@gmail.com>
2016-09-07 22:54:19 -0400

    pep8ified

--- HW2_fb55/show_bus_location_fb55.py
+++ HW2_fb55/show_bus_location_fb55.py
@@ -5,28 +5,47 @@ import sys
 import urllib2
 import json
 
+DEBUG = False  # change to True for debugging statements
 
 if __name__ == '__main__':
+    '''Displays the location of all MTA busses currently running for a given bus line.
+
+    Arguments: 
+        MTA key
+        Bus line
+
+    Output:
+        print to screen
+    '''
 
     if not len(sys.argv) == 3:
-        print ('''USAGE:
+        print('''USAGE:
         $python show_bus_locations.py <MTA_KEY> <BUS_LINE>''')
         sys.exit()
-    key = sys.argv[1]
-    bus_line = sys.argv[2]
-    print ('BUS LINE:', bus_line)
+    key, bus_line = sys.argv[1:]
+    print('BUS LINE:', bus_line)
 
-    this_url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=' + key + \
-               '&VehicleMonitoringDetailLevel=calls&LineRef='+bus_line
-#   print (this_url)
-    response = urllib2.urlopen(this_url)
-    data = json.load(response)
-    nbusses = len(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
+    bus_url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=' + key + \
+               '&VehicleMonitoringDetailLevel=calls&LineRef=' + bus_line
 
-    print ('Number of Active Buses : %d' % nbusses)
+    if DEBUG:
+        print("the url is:", bus_url)
+
+    # queries the url for json format bus data
+    response = urllib2.urlopen(bus_url)
+    busdata = json.load(response)
+
+    # counting the number of active busses
+    nbusses = len(busdata['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]\
+                  ['VehicleActivity'])
+
+    print('Number of Active Buses : %d' % nbusses)
+
+    # extracting and printing info for each bus
     for i in range(nbusses):
-        bi = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][i]\
-            ['MonitoredVehicleJourney']['VehicleLocation']
-#       print (bi)
-        print ('Bus %d is at latitude %f and longitude %f' % (i, bi['Latitude'], bi['Longitude']))
+        busi = busdata['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]\
+               ['VehicleActivity'][i]['MonitoredVehicleJourney']['VehicleLocation']
 
+        print('Bus %d is at latitude %f and longitude %f' % (i,
+                                                              busi['Latitude'],
+                                                              busi['Longitude']))
