1ccafbec0894820bcb595307786832ccaa6efef7
federica <fedhere@gmail.com>
2016-10-24 23:59:30 -0400

    Update readIncomeXls.py

--- midterm/readIncomeXls.py
+++ midterm/readIncomeXls.py
@@ -3,7 +3,7 @@ import pandas as pd
 def readIncomeXls(url):
     return pd.read_excel(url, header=3, index_col="ZIP\ncode [1]")
 
-# Even after reading it in with this function 
+# After reading it in with this function 
 # this file requires some serious wrangling. 
 # The zipcode is going to be read in as the index, but not all indeces are valid zipcodes.
 # Transform the zipcode to a number by using pd.to_numeric(... errors='coerce')
